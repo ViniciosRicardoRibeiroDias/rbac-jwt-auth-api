64688cf157bc88da457b632446ae4e63e25be62c
RicardoPato71 <s1llydogyai@gmail.com>
2026-04-12 10:00:19 -0300

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { Developer } from './developers/entities/developer.entity';
 import { Game } from './games/entities/game.entity';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
@@ -17,16 +17,26 @@ import { RolesGuard } from './users/user-roles.guard';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5433,
-            username: 'user',
-            password: 'pswd',
-            database: 'gamesDb',
-            entities: [Game, Developer, User],
-            synchronize: true,
+        ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                console.log('USERNAME:', configService.get('DB_USERNAME'));
+                console.log('PASSWORD:', configService.get('DB_PASSWORD'));
+                console.log(configService);
+
+                return {
+                    type: 'postgres',
+                    host: configService.get('DB_HOST'),
+                    port: Number(configService.get('DB_PORT')),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                    entities: [Game, Developer, User],
+                    synchronize: true,
+                };
+            },
         }),
         GamesModule,
         AuthModule,
